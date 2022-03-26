cd4158fe3e05fe1d90562c7cdb279d67b80a97f7
Daniel Vadil <dvadz@yahoo.com>
2022-03-26 00:18:43 -0400

    Rendered each item in an semantic-ui accordion with more styling

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,7 +1,21 @@
 import React from "react";
 
 const Accordion = ({ items }) => {
-  return <div>Accordion</div>;
+  const renderItems = items.map((item) => {
+    return (
+      <div key={item.title}>
+        <div className='title active'>
+          <i className='dropdown icon'></i>
+          {item.title}
+        </div>
+        <div className='content active'>
+          <p>{item.content}</p>
+        </div>
+      </div>
+    );
+  });
+
+  return <div className='ui styled accordion'>{renderItems}</div>;
 };
 
 export default Accordion;
