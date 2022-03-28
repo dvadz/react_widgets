7cd3022b3a39b8b014e8b9c2d249d0d24d82b385
Daniel Vadil <dvadz@yahoo.com>
2022-03-28 16:57:57 -0400

    Added a button that opens a search result in a new tab

--- src/components/Search.js
+++ src/components/Search.js
@@ -29,6 +29,14 @@ const Search = () => {
   const renderedResults = results.map((result) => {
     return (
       <div className='item' key={result.pageid}>
+        <div className='right floated content'>
+          <a
+            className='ui button'
+            href={`https://en.wikipedia.org/?curid=${result.pageid}`}
+            target='_blank'>
+            Go
+          </a>
+        </div>
         <div className='content'>
           <div className='header'>{result.title}</div>
           <div dangerouslySetInnerHTML={{ __html: result.snippet }}></div>
