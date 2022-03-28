165b45e9f537884e3ea8e5c8130ba6d02d8d2307
Daniel Vadil <dvadz@yahoo.com>
2022-03-27 22:47:48 -0400

    Added a state called "term" that will hold the search term

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 
 const Search = () => {
+  const [term, setTerm] = useState("");
+
   return (
     <div>
       <div className='ui form'>
         <div className='field'>
           <label>Enter Search Term</label>
-          <input className='input'></input>
+          <input value={term} className='input'></input>
         </div>
       </div>
     </div>
