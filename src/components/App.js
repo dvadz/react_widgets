29fa62f11a164f2f22bfc3eeb05b263b3197071d
Daniel Vadil <dvadz@yahoo.com>
2022-03-29 08:43:03 -0400

    Destructured props and removed redundant import of useState

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import Accordion from "./Accordion";
 import Search from "./Search";
 import Dropdown from "./Dropdown";
-import { useState } from "react";
 
 const items = [
   { title: "What is React?", content: "React is a front-end javascript framework" },
