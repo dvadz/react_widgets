16e902ca6c4f86cdfd3ab1b5dd49e583b4921424
Daniel Vadil <dvadz@yahoo.com>
2022-03-29 08:32:09 -0400

    Mapped the options to create a list

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,6 +1,14 @@
 import React from "react";
 
-const Dropdown = (props) => {
+const Dropdown = ({ options }) => {
+  const renderedOptions = options.map((option) => {
+    return (
+      <div key={option.value} className='item'>
+        {option.label}
+      </div>
+    );
+  });
+
   return <div>Dropdown</div>;
 };
 
