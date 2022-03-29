654c0a426028485de74dadfb719d41c53288cef4
Daniel Vadil <dvadz@yahoo.com>
2022-03-29 08:33:06 -0400

    Created a dropdown using semantic-ui and added in the renderedOptions

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -9,7 +9,18 @@ const Dropdown = ({ options }) => {
     );
   });
 
-  return <div>Dropdown</div>;
+  return (
+    <div className='ui form'>
+      <div className='field'>
+        <label className='label'>Select a Color</label>
+        <div className='ui selection dropdown visible active'>
+          <i className='dropdown icon'></i>
+          <div className='text'>Select Color</div>
+          <div className='menu visible transition'>{renderedOptions}</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dropdown;
