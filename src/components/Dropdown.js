8024b57c6553485b09b82a49b54cdb950193fb58
Daniel Vadil <dvadz@yahoo.com>
2022-03-29 15:10:06 -0400

    Created a state that determine if the selection is open or close

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Dropdown = ({ options, selectedColor, onSelectChange }) => {
+  const [open, setOpen] = useState(false);
+
   const renderedOptions = options.map((option) => {
     if (selectedColor === option) return null;
 
