a6c817077c8bacf7205238e4a753f4b816e45031
Daniel Vadil <dvadz@yahoo.com>
2022-03-29 23:21:21 -0400

    Imported Dropdown into the Translate component and passed down all needed props

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -1,7 +1,26 @@
 import React, { useState, useRef } from "react";
+import Dropdown from "./Dropdown";
+
+const options = [
+  { label: "Africaans", value: "af" },
+  { label: "Arabic", value: "ar" },
+  { label: "Hindi", value: "hi" },
+];
 
 const Translate = () => {
-  return <div>Translate</div>;
+  const [language, setLanguage] = useState(options[0]);
+
+  return (
+    <div>
+      <Dropdown
+        label='Select a language'
+        languages
+        options={options}
+        selected={language}
+        onSelectChange={setLanguage}
+      />
+    </div>
+  );
 };
 
 export default Translate;
