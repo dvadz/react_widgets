8d95b029bea1d87e0bb62f63e510b2bfc750e98e
Daniel Vadil <dvadz@yahoo.com>
2022-03-30 01:49:27 -0400

    Created a Convert component that will run google translate queries

---
+++ src/components/Convert.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from "react";
+
+const Convert = ({ text, language, setTranslation }) => {
+  useEffect(() => {}, [text, language]);
+  console.log("New language or text!");
+  return <div></div>;
+};
+
+export default Convert;
