8b4a0486d3d02ae07c073254622c79ce7b9c7ea6
Daniel Vadil <dvadz@yahoo.com>
2022-03-29 23:30:27 -0400

    Added an input and set it's value to state

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -9,9 +9,16 @@ const options = [
 
 const Translate = () => {
   const [language, setLanguage] = useState(options[0]);
+  const [text, setText] = useState("");
 
   return (
     <div>
+      <input
+        value={text}
+        onChange={(e) => {
+          setText(e.target.value);
+        }}
+      />
       <Dropdown
         label='Select a language'
         languages
